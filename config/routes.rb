6e658da33e01ec09bbda641af1f219c0564ff618
Mariana Cid <cidmariana5@gmail.com>
2020-07-15 21:13:25 -0600

    items/merchant routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
-      resources :items, except: [:new, :edit]
       resources :merchants, except: [:new, :edit] do 
         get 'items', to: 'merchants/items#index'
+      end
+      resources :items, except: [:new, :edit] do
+        get 'merchant', to: 'items/merchant#show'
       end
     end
   end
