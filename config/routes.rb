95a24c9508674ff881c4bf06af174ad60883b2ec
Mariana Cid <cidmariana5@gmail.com>
2020-07-15 20:25:24 -0600

    route for merchant item created

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       resources :items, except: [:new, :edit]
-      resources :merchants, except: [:new, :edit]
+      resources :merchants, except: [:new, :edit] do 
+        get 'items', to: 'merchants/items#index'
+      end
     end
   end
 end
