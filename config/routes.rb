3705b71a6bf3d405e06ddd1854efe3e45c38cabf
Mariana Cid <cidmariana5@gmail.com>
2020-07-15 23:15:25 -0600

    find route request spec

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
       resources :merchants, except: [:new, :edit] do 
         get 'items', to: 'merchants/items#index'
       end
+      namespace :items do
+        get 'find', to: 'search#show'
+      end
       resources :items, except: [:new, :edit] do
         get 'merchant', to: 'items/merchant#show'
       end
