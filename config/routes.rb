6d3ee66bb55c2b3fac25abdd339e1e1e98c6184f
Mariana Cid <cidmariana5@gmail.com>
2020-07-16 00:24:03 -0600

    find/find_all routes for merchant search controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
+      namespace :merchants do
+        get 'find', to: 'search#show'
+        get 'find_all', to: 'search#index'
+      end
       resources :merchants, except: [:new, :edit] do 
         get 'items', to: 'merchants/items#index'
       end
