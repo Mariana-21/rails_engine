7fa88c7a0e9692b46743e1d5e83926791c03a517
Mariana Cid <cidmariana5@gmail.com>
2020-07-16 00:24:20 -0600

    merchant search controller

---
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::Merchants::SearchController < ApplicationController
+  def index
+    results = Search.new.results(Merchant, query_params)
+    render json: MerchantSerializer.new(Merchant.find(results))
+  end
+
+  def show
+    result = Search.new.results(Merchant, query_params).first
+    render json: MerchantSerializer.new(Merchant.find_by(id: result))
+  end
+
+  private
+
+  def query_params
+    query_keys = params.keys[0..-3]
+    query_values = []
+    query_keys.each { |key| query_values << params[key] }
+    query_keys.zip(query_values)
+  end
+end 
